4a41260a7f15b9a8fe64a13f2d970af2d91ea29b
Russell Purinton <russell.purinton@gmail.com>
2023-08-28 10:27:03 -0400

    x

--- index.php
+++ index.php
@@ -5,24 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css%22%3E
     <link rel="stylesheet" href="/assets/css/dark-theme.css">
+    <style>
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <h1>Login</h1>
-        <form id="loginForm">
-            <div class="mb-3">
-                <label for="username" class="form-label">Username</label>
-                <input type="text" class="form-control" id="username" name="username" required>
+        <div class="card card-login">
+            <div class="card-body">
+                <h1>Login</h1>
+                <form id="loginForm">
+                    <div class="mb-3">
+                        <label for="username" class="form-label">Username</label>
+                        <input type="text" class="form-control" id="username" name="username" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" id="password" name="password" required>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Login</button>
+                </form>
             </div>
-            <div class="mb-3">
-                <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Login</button>
-        </form>
+        </div>
 
         <div class="mt-3">
             <p>Don't have an account? <a href="signup.html">Create Account</a></p>
@@ -33,7 +39,7 @@
     <script>
         // Add JavaScript code to handle the login functionality here
         document.getElementById('loginForm').addEventListener('submit', function(event) {
-            event.preventDefault();
+event.preventDefault();
             // Perform the login logic here (e.g., validate credentials, make API requests)
             // Redirect to the appropriate page if login is successful
         });
