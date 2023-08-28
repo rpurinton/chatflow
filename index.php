ab4bd02d93762e011af4578514a0612b3f48947c
Russell Purinton <russell.purinton@gmail.com>
2023-08-28 11:02:18 -0400

    Update index.php

--- index.php
+++ index.php
@@ -18,12 +18,10 @@
                 <h1>Login</h1>
                 <form id="loginForm">
                     <div class="mb-3">
-                        <label for="username" class="form-label">Username</label>
-                        <input type="text" class="form-control" id="username" name="username" required>
+                        <input type="text" class="form-control" id="username" name="username" required autofocus autocomplete="off" placeholder="Username">
                     </div>
                     <div class="mb-3">
-                        <label for="password" class="form-label">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" required>
+                        <input type="password" class="form-control" id="password" name="password" required autocomplete="off" placeholder="Password">
                     </div>
                     <button type="submit" class="btn btn-primary">Login</button>
                 </form>
