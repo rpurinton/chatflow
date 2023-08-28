51ae590b565d6c2bb00f69f6a840c39e43063380
Russell Purinton <russell.purinton@gmail.com>
2023-08-28 11:11:29 -0400

    x

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
+    <title>ChatFlow</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="/assets/css/dark-theme.css">
     <style>
@@ -13,29 +13,7 @@
 
 <body>
     <div class="container">
-        <div class="card card-login">
-            <div class="card-body">
-                <h1>Login</h1>
-                <form id="loginForm">
-                    <div class="mb-3">
-                        <input type="text" class="form-control" id="username" name="username" required autofocus autocomplete="off" placeholder="Username">
-                    </div>
-                    <div class="mb-3">
-                        <input type="password" class="form-control" id="password" name="password" required autocomplete="off" placeholder="Password">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Login</button>
-                </form>
-            </div>
-            <div class="icon-container">
-                <div class="icon"></div>
-                <p class="icon-title">ChatFlow</p>
-            </div>
-        </div>
 
-        <div class="mt-3">
-            <p>Don't have an account? <a href="signup.html">Create Account</a></p>
-            <p>Forgot your password? <a href="forgot-password.html">Reset Password</a></p>
-        </div>
     </div>
 
     <script>
