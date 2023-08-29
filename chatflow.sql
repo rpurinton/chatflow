bba8356a95ba61378420bfe33e92e9a0d4531194
Russell Purinton <russell.purinton@gmail.com>
2023-08-29 04:21:13 -0400

    Update chatflow.sql

--- chatflow.sql
+++ chatflow.sql
@@ -1,31 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Aug 29, 2023 at 08:11 AM
--- Server version: 10.5.16-MariaDB
--- PHP Version: 8.2.9
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
-;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
-;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
-;
-/*!40101 SET NAMES utf8mb4 */
-;
---
--- Database: `chatflow`
---
-
--- --------------------------------------------------------
---
--- Table structure for table `api_tokens`
---
-
 CREATE TABLE `api_tokens` (
     `token_id` bigint(20) NOT NULL,
     `token` varchar(255) NOT NULL,
@@ -348,10 +320,3 @@ ADD CONSTRAINT `collections_chatgpt_keys_ibfk_1` FOREIGN KEY (`collection_id`) R
 --
 ALTER TABLE `sessions`
 ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`collection_id`) REFERENCES `collections` (`collection_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
-;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
-;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
-;
