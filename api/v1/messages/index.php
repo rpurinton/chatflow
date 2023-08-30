d964c6dd2d0492106b9bf78cc7a1e421b86830eb
Russell Purinton <russell.purinton@gmail.com>
2023-08-30 03:59:58 -0400

    x

--- api/v1/messages/index.php
+++ api/v1/messages/index.php
@@ -1,9 +1,13 @@
 <?php
+// Look for a token in the HTTP Authorization header
 if (!isset($_SERVER["HTTP_AUTHORIZATION"])) error(400, "No token specified. You must specify a token in the HTTP Authorization header.");
-$token = $_SERVER["HTTP_AUTHORIZATION"];
+
+// Validate the token
+$token = str_replace("Bearer ", "", $_SERVER["HTTP_AUTHORIZATION"]);
 if (strlen($token) != 64) error(400, "Invalid token length. Must be 64 characters.");
 if (substr($token, 0, 9) != "chatflow-") error(400, "Invalid token prefix. Must be 'chatflow-...'.");
 
+// Connect to the database
 try {
     require_once(__DIR__ . "/../../../SqlClient.php");
     $sql = new RPurinton\ChatFlow\SqlClient();
@@ -15,19 +19,49 @@ try {
     error(500, $e->getMessage());
 }
 
+// Continue to Validate the token
 $token = $sql->escape($token);
 extract($sql->single("SELECT count(1) as `valid`, `token_id`, `token` FROM `api_tokens` WHERE `token` = '$token'"));
 if (!$valid) error(401, "Invalid token. Check your token and try again.");
 if (!$token_id) error(401, "Invalid token. Check your token and try again.");
 
+// Continue to Validate the request
 $response = [];
+
+// Validate the request method
 if ($_SERVER["REQUEST_METHOD"] != "POST") error(400, "Invalid request method. Must be POST, not " . $_SERVER["REQUEST_METHOD"]);
+
+// Validate the request body
 $json_string_input = file_get_contents("php://input");
+
+// Validate the JSON string
 if (!$json_string_input) error(400, "No input data. You must POST a valid JSON string in the body.");
 if (!($json_input = json_decode($json_string_input, true))) error(400, "The string in body cannot be decoded as JSON. Check your syntax and try again.");
+
+// Validate the session
 if (!isset($json_input["session"])) error(400, "No session specified. You must specify a session ID or 'new' to create a new session.");
 if ($json_input["session"] == "new") {
+    // Create a new session
+    // Validate the collection
     if (!isset($json_input["collection"])) error(400, "No collection specified. You must specify a collection ID or 'new' to create a new collection.");
+    if ($json_input["collection"] == "new") {
+        // Create a new collection
+        $sql->query("INSERT INTO `collections` () VALUES ()");
+        $collection_id = $sql->insert_id();
+        $sql->query("INSERT INTO `collections_api_tokens` (`collection_id`, `token_id`) VALUES ('$collection_id', '$token_id')");
+        $sql->query("INSERT INTO `sessions` (`collection_id`) VALUES ('$collection_id')");
+        $session_id = $sql->insert_id();
+    } else {
+        // Use an existing collection
+        $collection_id = $json_input["collection"];
+        if (!is_numeric($collection_id)) error(400, "Invalid collection ID. Must be numeric.");
+        extract($sql->single("SELECT count(1) as `valid` FROM `collections` WHERE `collection_id` = '$collection_id'"));
+        if (!$valid) error(400, "Invalid collection ID. Check your collection ID and try again.");
+        extract($sql->single("SELECT count(1) as `valid` FROM `collections_api_tokens` WHERE `collection_id` = '$collection_id' AND `token_id` = '$token_id'"));
+        if (!$valid) error(401, "Invalid token for this collection. Check your token and try again.");
+        $sql->query("INSERT INTO `sessions` (`collection_id`) VALUES ('$collection_id')");
+        $session_id = $sql->insert_id();
+    }
     $collection_id = $json_input["collection"];
     if (!is_numeric($collection_id)) error(400, "Invalid collection ID. Must be numeric.");
     extract($sql->single("SELECT count(1) as `valid` FROM `collections` WHERE `collection_id` = '$collection_id'"));
@@ -44,6 +78,7 @@ if ($json_input["session"] == "new") {
     extract($sql->single("SELECT count(1) as `valid` FROM `collections_api_tokens` WHERE `collection_id` = (SELECT `collection_id` FROM `sessions` WHERE `session_id` = '$session_id') AND `token_id` = '$token_id'"));
     if (!$valid) error(401, "Invalid token for this session. Check your token and try again.");
 }
+
 $stream = isset($json_input["stream"]) && $json_input["stream"] === true ? true : false;
 if ($stream) {
     header('Content-Type: plain/text; charset=utf-8');
