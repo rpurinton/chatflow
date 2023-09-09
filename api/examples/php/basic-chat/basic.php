08d97c52754a10cd3b4110322f487736ed691697
Russell Purinton <russell.purinton@gmail.com>
2023-09-09 05:26:55 -0400

    Update basic.php

--- api/examples/php/basic-chat/basic.php
+++ api/examples/php/basic-chat/basic.php
@@ -35,6 +35,8 @@ if (!isset($response["choices"][0]["message"]["content"])) die("Unable to find r
 // print the response text and input prompt
 echo ("Press CTRL+C to exit...\n" . $response["choices"][0]["message"]["content"] . "\n> ");
 
+passthru("say " . $response["choices"][0]["message"]["content"]);
+
 // loop until the user exits
 while (true) {
     // open a stream for the user input
@@ -76,6 +78,8 @@ while (true) {
     $fp = fopen($url, "r", false, $context) or die("Unable to open stream!");
 
     // read the stream until the end
+
+    $full_response = "";
     while (!feof($fp)) {
         // get a character from the stream
         $char = fgetc($fp);
@@ -83,9 +87,16 @@ while (true) {
         // remove null characters
         $char = str_replace("\0", "", $char);
 
+        // append the char
+        $full_response .= $char;
+
+        $full_response = check_and_say($full_response);
+
         // print the character
         echo $char;
     }
+
+    $full_response = say($full_response);
 
     // close the stream
     fclose($fp);
@@ -98,4 +109,130 @@ while (true) {
     flush();
 
     // loop!
+}
+
+
+function check_and_say($full_response): string
+{
+    $delimiters = [
+        ". ",
+        "! ",
+        "? ",
+        ".\n",
+        "!\n",
+        "?\n",
+    ];
+    // while the response contains a delimiter
+    while (strpos($full_response, $delimiters[0]) !== false || strpos($full_response, $delimiters[1]) !== false || strpos($full_response, $delimiters[2]) !== false || strpos($full_response, $delimiters[3]) !== false || strpos($full_response, $delimiters[4]) !== false || strpos($full_response, $delimiters[5]) !== false) {
+        // get the first delimiter
+        $delimiter = $delimiters[0];
+
+        // get the position of the first delimiter
+        $pos = strpos($full_response, $delimiter);
+
+        // if the first delimiter is not found
+        if ($pos === false) {
+            // get the next delimiter
+            $delimiter = $delimiters[1];
+
+            // get the position of the next delimiter
+            $pos = strpos($full_response, $delimiter);
+        }
+
+        // if the next delimiter is not found
+        if ($pos === false) {
+            // get the next delimiter
+            $delimiter = $delimiters[2];
+
+            // get the position of the next delimiter
+            $pos = strpos($full_response, $delimiter);
+        }
+
+        // if the next delimiter is not found
+        if ($pos === false) {
+            // get the next delimiter
+            $delimiter = $delimiters[3];
+
+            // get the position of the next delimiter
+            $pos = strpos($full_response, $delimiter);
+        }
+
+        // if the next delimiter is not found
+        if ($pos === false) {
+            // get the next delimiter
+            $delimiter = $delimiters[4];
+
+            // get the position of the next delimiter
+            $pos = strpos($full_response, $delimiter);
+        }
+
+        // if the next delimiter is not found
+        if ($pos === false) {
+            // get the next delimiter
+            $delimiter = $delimiters[5];
+
+            // get the position of the next delimiter
+            $pos = strpos($full_response, $delimiter);
+        }
+
+        // if the next delimiter is not found
+        if ($pos === false) {
+            // break out of the loop
+            break;
+        }
+
+        // get the text before the delimiter
+        $text = substr($full_response, 0, $pos + strlen($delimiter));
+
+        // remove the text before the delimiter
+        $full_response = substr($full_response, $pos + strlen($delimiter));
+
+        // remove the delimiter
+        $full_response = str_replace($delimiter, "", $full_response);
+
+        // if the text is not empty
+        if (!empty($text)) {
+            // say the text
+            say($text);
+        }
+    }
+    return $full_response;
+}
+
+function say($text)
+{
+    // remove any characters that will mess up the bash cli
+    $text = str_replace("\"", "", $text);
+    $text = str_replace("'", "", $text);
+    $text = str_replace("`", "", $text);
+    $text = str_replace("$", "", $text);
+    $text = str_replace("(", "", $text);
+    $text = str_replace(")", "", $text);
+    $text = str_replace(";", "", $text);
+    $text = str_replace("&", "", $text);
+    $text = str_replace("|", "", $text);
+    $text = str_replace(">", "", $text);
+    $text = str_replace("<", "", $text);
+    $text = str_replace("!", "", $text);
+    $text = str_replace("?", "", $text);
+    $text = str_replace("*", "", $text);
+    $text = str_replace("=", "", $text);
+    $text = str_replace("[", "", $text);
+    $text = str_replace("]", "", $text);
+    $text = str_replace("{", "", $text);
+    $text = str_replace("}", "", $text);
+    $text = str_replace("\\", "", $text);
+    $text = str_replace("/", "", $text);
+    $text = str_replace(":", "", $text);
+    $text = str_replace("@", "", $text);
+    $text = str_replace("#", "", $text);
+    $text = str_replace("%", "", $text);
+    $text = str_replace("^", "", $text);
+    $text = str_replace("+", "", $text);
+    $text = str_replace("-", "", $text);
+    $text = str_replace("_", "", $text);
+    $text = str_replace("~", "", $text);
+    $text = str_replace(",", "", $text);
+    $text = str_replace(".", "", $text);
+    passthru("say \"" . $text . "\"");
 }
